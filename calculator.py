8402ff5edf6320a00c938a14eb99a60811ecdf5e
MaidaNadeem26 <maidanadeem99@gmail.com>
2026-04-23 12:31:24 +0500

    Add files via upload
    add project files

---
+++ calculator.py
@@ -0,0 +1,13 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 1: # defect here 
+        return "Error"
+    return a / b
